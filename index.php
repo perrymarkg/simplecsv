b84f4ed6d1c9720147ed6b525328fa6a3df7d62f
perry <User@Tipzee-Aspire.(none)>
2017-12-03 09:41:08 +0800

    Modify output

--- index.php
+++ index.php
@@ -3,11 +3,19 @@
 require_once 'vendor/autoload.php';
 use prymag\SimpleCSV\CSVReader;
 
-$v = new CSVReader( 'Sample.csv', true );
+$csv = new CSVReader( 'Sample.csv', true );
 
-$arr = $v->read()->getData();
-//var_dump($arr);
+$array = $csv->read()->getData();
 
-foreach($arr as $a){
-    echo $a['State'] . "<br/>";
+foreach($array as $item){
+    if( is_array($item) ){
+        foreach($item as $key => $val){
+            echo $key . ' : ' . $val . '<br/>';
+        }
+    }
+    else {
+        echo 'Invalid';
+    }
+    echo '<hr/>';
 }
+echo 'Total' . $v->getTotal();
